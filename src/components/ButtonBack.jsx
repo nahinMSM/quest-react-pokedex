cf60c9af1e71e1cb47bd47a9e7d3eeb49c919cfb
nahinMSM <nahin_msm@hotmail.com>
2024-10-02 10:45:58 -0300

    Aplicação Pokedex

--- src/components/ButtonBack.jsx
+++ src/components/ButtonBack.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom'
-import styled from '@babel/core'
+import styled from 'styled-components'
 import  PokeBall  from '../assets/poke-ball.png'
 
 export const ButtonToBack = () => {
